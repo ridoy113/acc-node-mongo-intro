b68f49a1d7d4b59befdf009478be852d090023cb
ridoy113 <almamounhridoy113@gmail.com>
2023-01-18 11:34:46 +0600

    use js setTimeout

--- SyncAsync.js
+++ SyncAsync.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// start
+
+console.log("Hello World 1");
+
+setTimeout(() => {
+  const user = { id: 1 };
+
+  setTimeout(() => {
+    const products = [{}, {}, {}];
+
+    setTimeout(() => {
+      const price = [];
+    }, 1000);
+  }, 500);
+}, 1000);
+
+console.log("Hello World 3");
